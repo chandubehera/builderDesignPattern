db396cf35aa67df278f9683f0aa31b1ba149b913
Chandrakanta Behera <chandrakantab@maveric-systems.com>
2025-02-28 23:42:54 +0530

    builder design pattern

---
+++ src/main/java/com/dp/test/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.dp.test.repository;
+
+import com.dp.test.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+
+}
